06163f07bff4c9a95e28fe209ce9d793f3f2a1a6
Nilatti <alisha.huber@gmail.com>
2013-04-09 09:59:04 -0400

    created Rails app

---
+++ Henslowe/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henslowe::Application.config.secret_token = 'd73c07057af025a6be453fc8ff91fcc501729c19055e92ea9cfaf3ece588d75bed1561ea02dadf2e1349c53293ebfe8d71b1b2564968c7ef7cd4d7e29d83510a'
